4cd1f0987dc87a3f2c5b9e70678017d68ad72077
Seyi Jacobs-Obalakun <34285589+seyiobalakun@users.noreply.github.com>
2018-04-24 22:07:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HNG Internship 4.0 | Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="container">
+		<div class="img">
+			<nav>
+				<ul>
+					<li class="current">Home</li>
+					<li>About Us</li>
+					<li>Services</li>
+					<li>Contact Us</li>
+				</ul>
+			</nav>
+			<div class="msg_area">
+				<span class="welcome">WELCOME</span>
+				<br><br><br>
+				<p class="to">TO</p>
+				<br><br>
+				<p><span class="hng"><span class="hng_text">HNG</span> INTERNSHIP</span> <span class="edition">4.0</span></p>
+			</div>
+			<div class="time">
+				<span class="date">DATE</span>
+				<span class="day"><?php
+									echo date('D');
+									?></span>
+				<span id="month"><?php
+										echo date('M');
+									?></span>
+				<span class="year"><?php
+										echo date('Y');
+									?></span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HNG Internship - Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="container">
+		<div class="img">
+			<nav>
+				<ul>
+					<li class="current">Home</li>
+					<li>About Us</li>
+					<li>Services</li>
+					<li>Contact Us</li>
+				</ul>
+			</nav>
+			<div class="msg_area">
+				<span class="welcome">WELCOME</span>
+				<br><br><br>
+				<p class="to">TO</p>
+				<br><br>
+				<p><span class="hng"><span class="hng_text">HNG</span> INTERNSHIP</span> <span class="edition">4.0</span></p>
+			</div>
+			<div class="time">
+				<span class="date">DATE</span>ou ac
+				<span class="day">23</span>
+				<span id="month">04</span>
+				<span class="year">2018</span>
+			</div>
+		</div>
+	</div>
+</body>
+</html> -->
